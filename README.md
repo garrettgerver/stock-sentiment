ebef099e30770a44359ceb6e94082471538b9906
garrettgerver <garrett.gerver@gmail.com>
2024-10-18 11:41:40 -0400

    Restructured .py and added requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# stock-sentiment
+# Stock Sentiment Script in Python
+This is a simple python script that uses a pretrained LLM model [FinancialBERT-Sentiment-Analysis](https://huggingface.co/ahmedrachid/FinancialBERT-Sentiment-Analysis) to calculate the general sentiment of a given stock per the most recent Yahoo Finance articles about said stock.
+
+The current way to run this script as per `python stock-sentiment -h`:
+```
+usage: stock-sentiment.py [-h] [-s {Yahoo}] [-f FILE] ticker [ticker ...]
+
+positional arguments:
+  ticker                ticker symbols
+
+options:
+  -h, --help            show this help message and exit
+  -s {Yahoo}, --source {Yahoo}
+                        source of news articles (default: Yahoo)
+  -f FILE, --file FILE  file to write to (.txt, .json)
+```
+
+This script can also be imported as a module and returns the results as a JSON object.
+
+Current points of development are timing out requests if they take too long and the ability to pass through alternative websites to Yahoo Finance that may potentially yield more acurate results, such as Barron's. 
--- stock-sentiment.py
+++ stock-sentiment.py
@@ -1,4 +1,5 @@
-import sys
+import json
+import argparse
 import feedparser
 import requests
 from xml.sax import SAXParseException
@@ -7,37 +8,57 @@ from bs4 import BeautifulSoup
 from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import pipeline
 
-if len(sys.argv) < 2:
-    raise AssertionError("No arguments passed")
+model = BertForSequenceClassification.from_pretrained(
+    "ahmedrachid/FinancialBERT-Sentiment-Analysis",
+    num_labels=3
+)
+tokenizer = BertTokenizer.from_pretrained(
+    "ahmedrachid/FinancialBERT-Sentiment-Analysis", 
+    clean_up_tokenization_spaces=True
+)
+nlp = pipeline("text-classification", model=model, tokenizer=tokenizer)
 
-def main():
-    model = BertForSequenceClassification.from_pretrained(
-            "ahmedrachid/FinancialBERT-Sentiment-Analysis",
-            num_labels=3)
-    tokenizer = BertTokenizer.from_pretrained(
-            "ahmedrachid/FinancialBERT-Sentiment-Analysis", 
-            clean_up_tokenization_spaces=True)
-    nlp = pipeline("text-classification", model=model, tokenizer=tokenizer)
+class ModelObject():
+    def __init__(self):
+        self._container: dict[str, [dict[str, float]]] = {}
+        self._elements: dict[str, float] = {}
 
-    tickers = [x.upper() for x in sys.argv[1:]]
+    @property
+    def get_article_sentiments(self) -> dict[str, float]:
+        return self._elements
+
+    @property
+    def get_ticker_sentiments(self) -> dict[str, [dict[str, float]]]:
+        return self._container
+
+    def add_article_sentiment(self, article: str, sentiment: float) -> None:
+        self._elements.update({article: sentiment})
+
+    def add_ticker_sentiment(self, ticker: str, sentiment: dict[str, float]) -> None:
+        self._container.update({ticker: self._elements})
+        self._elements = {}
+
+def model(tickers: list[str]) -> ModelObject: 
+    data = ModelObject()
     for ticker in tickers:
         visited_links = []
-        rss_url = f"https://feeds.finance.yahoo.com/rss/2.0/headline?s={ticker}"
+        match (args.source):
+            case "Yahoo": rss_url = f"https://feeds.finance.yahoo.com/rss/2.0/headline?s={ticker}"
+            case _: break #will be used in future for incorporating more websites
         try:
             feed = feedparser.parse(rss_url)
             if feed.status != 200:
                 if feed.status == 404:
-                    print(f"No ticker {ticker}. Proceeding.. ")
+                    print(f"No ticker {ticker}. Proceeding.. \n")
                 else:
-                    print(f"Unknown error for {ticker}. Proceeding.. ")
+                    print(f"Unknown error for {ticker}. Proceeding.. \n")
                 continue
         except SAXParseException:
-            print(f"Error parsing RSS feed for {ticker}. Proceeding.. ")
+            print(f"Error parsing RSS feed for {ticker}. Proceeding.. \n")
             continue
 
         for link in feed.entries:
             title = link.title
-            desc = link.description
             url = link.link
             if url in visited_links:
                 continue
@@ -45,18 +66,16 @@ def main():
             try:
                 req = requests.get(url)
             except MissingSchema:
-                print(f"Article not found. Proceeding.. ")
+                print(f"Article not found. Proceeding.. \n")
                 continue
             if req.status_code != 200:
                 if req.status_code == 404:
-                    print(f"Article not found. Proceeding.. ")
+                    print(f"Article not found. Proceeding.. \n")
                 else:
-                    print(f"Unknown error. Proceeding..")
+                    print(f"Unknown error. Proceeding.. \n")
                 continue
            
-            print(title)
-            text = ""
-            sentiments = 0
+            total_sentiment = 0
             soup = BeautifulSoup(req.content, "html.parser")
             for para in soup.find_all("p"):
                 text = BeautifulSoup(str(para), "lxml").text
@@ -66,9 +85,31 @@ def main():
                         (1 if sentiment_list[0]["label"]=="positive" else
                         -1 if sentiment_list[0]["label"]=="negative" else 0)
                 )
-                sentiments += sentiment
-            print(sentiments)
+                total_sentiment += sentiment
+            print(title)
+            print(f"{total_sentiment}\n")
+            data.add_article_sentiment(title, total_sentiment)
+        data.add_ticker_sentiment(ticker, data.get_article_sentiments)
+    return data.get_ticker_sentiments
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--source", choices=["Yahoo"], default="Yahoo", help="source of news articles"
+    )
+    parser.add_argument(
+        "-f", "--file", help="file to write to (.txt, .json)"
+    )
+    parser.add_argument("ticker", nargs="+", help="ticker symbols")
+    args = parser.parse_args()
+    if args.file:
+        if not(args.file.split(".")[-1] == "json" or "txt"):
+            raise AssertionError("Invalid file format (.txt, .json)")
 
+    tickers = [x.upper() for x in args.ticker] 
+    data = model(tickers)
+    if args.file:
+        with open(args.file, "w") as f:
+            json.dump(data, f, indent=4)
+            print(f"Wrote data to {args.file}")
+    print("Done")
